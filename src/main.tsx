6313135ca4aef600a5605548e72960123a8ac5c7
MohamedKhalilHermassi <mohamedkhalil.hermassi@esprit.tn>
2025-07-03 13:56:44 +0100

    minor changes

--- src/main.tsx
+++ src/main.tsx
@@ -14,7 +14,7 @@ const App = () => {
       confirmColor: "#dc2626",
       confirmTextFont: "monospace", // change with the font of your choice
       cancelTextFont: "monospace",
-      dialogTextFont :"monospace"
+      dialogTextFont :"sans-serif"
     })
 
     if (ok) {
